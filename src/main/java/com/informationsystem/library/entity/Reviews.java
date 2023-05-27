5fba74a9c218a6c53613ad0183ce44cf2267bbea
NormalMood <littlebigdiamond@gmail.com>
2023-05-27 15:00:17 +0300

    fix: change validation constraint package for entities

--- src/main/java/com/informationsystem/library/entity/Reviews.java
+++ src/main/java/com/informationsystem/library/entity/Reviews.java
@@ -2,14 +2,13 @@ package com.informationsystem.library.entity;
 
 import lombok.*;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 import java.sql.Date;
 
