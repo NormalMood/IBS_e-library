1a12c551001845812878e1ed3639699a27cb150d
NormalMood <littlebigdiamond@gmail.com>
2023-05-27 15:15:53 +0300

    fix: spring-security authorization

--- src/main/java/com/informationsystem/library/config/SecurityConfig.java
+++ src/main/java/com/informationsystem/library/config/SecurityConfig.java
@@ -7,16 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
 
     private final UserDetailsService employeeDetailsService;
 
@@ -24,31 +26,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig(@Qualifier("employeeDetailsServiceImpl") UserDetailsService userDetailsService){
         this.employeeDetailsService = userDetailsService;
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(APIRoutes.EMPLOYEE_BIN_CONTROLLER_MAPPING + "/**").hasAnyAuthority(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers("/api/library/**").hasAnyAuthority(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers("/api/user/**").hasAnyAuthority(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers(APIRoutes.ADMIN_CONTROLLER_MAPPING + "/**").hasAuthority(Role.ADMIN.name())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .and()
-                .formLogin(formLogin -> formLogin
-                        .successHandler(new CustomAuthenticationSuccessHandler()))
-                .httpBasic();
+    
+  //  @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	return http
+    			.authorizeHttpRequests(requests -> 
+    					requests
+    						.requestMatchers(APIRoutes.EMPLOYEE_BIN_CONTROLLER_MAPPING + "/**")
+    						.hasAnyAuthority(Role.ADMIN.name(), Role.USER.name())
+    						.requestMatchers("/api/library/**")
+    						.hasAnyAuthority(Role.ADMIN.name(), Role.USER.name())
+    						.requestMatchers("/api/user/**")
+    						.hasAnyAuthority(Role.ADMIN.name(), Role.USER.name())
+    		                .requestMatchers(APIRoutes.ADMIN_CONTROLLER_MAPPING + "/**")
+    		                .hasAuthority(Role.ADMIN.name())
+    						.anyRequest()
+    						.authenticated())
+    			.formLogin(form ->
+    					form
+    						.successHandler(new CustomAuthenticationSuccessHandler()))
+    			.logout(configurer ->
+    					configurer
+    						.logoutSuccessUrl("/login")
+    						.invalidateHttpSession(true)
+    						.clearAuthentication(true)
+    						.deleteCookies("JSESSIONID"))
+    			.authenticationProvider(authenticationProvider())
+    			.build();
     }
+    
+    //@Bean
+	public AuthenticationProvider authenticationProvider() { //compare user's parameters
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(employeeDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return authenticationProvider;
+	}
 
-    @Bean
+    //@Bean
     protected PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(16);
     }
